cb503ed3c1465fadd8f900ee741eaedd66369dff
desistmrym <desisitimaryam@gmail.com>
2025-01-25 01:40:37 +0700

    fix bug header floating

--- src/component/cover_header.jsx
+++ src/component/cover_header.jsx
@@ -3,7 +3,7 @@ import { header, header_1 } from "../assets/list_image";
 const CoverHeader = ({type}) => {
     const img = type === 'header_1' ? header_1 : header 
     return (
-        <img src={img} alt="" className={`object-cover w-[100%] h-[100%] mt-16 ${type === 'header_1' ? 'lg:-mt-2' : 'lg:mt-0'} lg:h-[60vh]`} />
+        <img src={img} alt="" className={`object-cover w-[100%] h-[100%] ${type === 'header_1' ? 'lg:-mt-2' : 'lg:mt-0'} lg:h-[60vh]`} />
     )
 }
 
