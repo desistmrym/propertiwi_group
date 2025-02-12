2b875494742c00118553206b90d7ffc32f26bcbc
desistmrym <desisitimaryam@gmail.com>
2025-02-12 12:46:25 +0700

    update cover header contact

--- src/component/cover_header.jsx
+++ src/component/cover_header.jsx
@@ -4,8 +4,8 @@ const CoverHeader = ({type, data}) => {
     const img = type === 'header_1' ? header_1 : data && "acf" in data &&  data.acf.header_images.length > 0 ? data.acf.header_images[0].url : header 
     return (
         <div className="relative flex items-center">
-            <img src={img} alt="" className={`object-cover w-[100%] h-[100%] pt-[4rem] lg:pt-0 ${type === 'header_1' ? 'lg:-mt-2' : 'lg:mt-0'} lg:h-[70vh]`} />
-            {type === 'header_1' ?
+            <img src={img} alt="" className={`object-center w-[100%] h-[100%] pt-[4rem] lg:pt-0 ${type === 'header_1' ? 'lg:-mt-2' : 'lg:mt-0'} lg:h-[70vh]`} />
+            {/* {type === 'header_1' ?
                 <div className="absolute lg:top-0 z-[8] px-5 lg:px-[4rem] pt-[4rem] lg:pt-[5rem] lg:py-8 flex items-center">
                     <div className="text-white">
                         <p className="text-[1rem] lg:text-[3.5rem] font-bold font-['Inter']">Ayo Gabung!</p>
@@ -17,7 +17,7 @@ const CoverHeader = ({type, data}) => {
                         </div>
                     </div>
                 </div>
-            : null }
+            : null } */}
         </div>
     )
 }
